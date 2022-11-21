e0d7a879b1c0207f0342885cb4e59f7613d8012e
Debora Serra <debora.r.serra@gmail.com>
2022-11-21 09:34:18 -0300

    feat: add req2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,9 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     for enter, leave in permanence_period:
-        if ((enter is None or leave is None) or (not isinstance(enter, int) or not isinstance(leave, int))):
+        if (enter is None or leave is None) or (
+            not isinstance(enter, int) or not isinstance(leave, int)
+        ):
             return None
         list = [index for index in range(enter, leave + 1)]
         if target_time in list:
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,35 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    wrong_key()
+    odd_key()
+    even_key()
+    raise_error_key()
+    raise_error_message()
+
+
+def raise_error_message():
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123456, 2)
+
+
+def raise_error_key():
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("AABBBCCC", "2")
+
+
+def wrong_key():
+    result = encrypt_message("AAABBBCCC", 10)
+    assert result == "CCCBBBAAA"
+
+
+def odd_key():
+    result = encrypt_message("AABBCC", 3)
+    assert result == "BAA_CCB"
+
+
+def even_key():
+    result = encrypt_message("ABBCCA", 4)
+    assert result == "AC_CBBA"
