e0d0873b256744baef0307cd0c8ca6968b2e4af5
Debora Serra <debora.r.serra@gmail.com>
2022-11-21 09:48:44 -0300

    feat: add req3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or low_index < 0 or high_index < 0:
+        return False
+    elif low_index == high_index:
+        return True
+    elif high_index >= (len(word) // 2):
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    elif word[low_index] == word[high_index]:
+        return True
+    else:
+        return False
+    
