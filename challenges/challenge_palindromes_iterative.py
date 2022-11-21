9fda955decf8bbaa089fad62e2cf7b8956dca48a
Debora Serra <debora.r.serra@gmail.com>
2022-11-21 09:55:50 -0300

    feat: add req6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,17 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    low_index, high_index = 0, len(word) - 1
+    is_palindrome = False
+    while is_palindrome == False:
+        if low_index == high_index:
+            is_palindrome = True
+        elif high_index >= (len(word) // 2):
+            low_index, high_index = low_index + 1, high_index - 1
+        elif word[low_index] == word[high_index]:
+            is_palindrome = True
+        else:
+            return False
+    return is_palindrome
+    
