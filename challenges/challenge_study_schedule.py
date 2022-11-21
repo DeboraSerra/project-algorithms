384b81132fc8f0217c02ae7702570f5a96b15569
Debora Serra <debora.r.serra@gmail.com>
2022-11-21 09:10:02 -0300

    feat: add req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    times = 0
+    if target_time is None:
+        return None
+    for enter, leave in permanence_period:
+        if ((enter is None or leave is None) or (not isinstance(enter, int) or not isinstance(leave, int))):
+            return None
+        list = [index for index in range(enter, leave + 1)]
+        if target_time in list:
+            times += 1
+    return times
