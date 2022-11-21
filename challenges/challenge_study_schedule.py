865f33cda1a0413bebf59f01a565aef6a8f0e782
Debora Serra <debora.r.serra@gmail.com>
2022-11-21 11:43:17 -0300

    try req1 again

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
     times = 0
-    if target_time is None:
+    if target_time is None or isinstance(target_time, tuple):
         return None
     for enter, leave in permanence_period:
         if (enter is None or leave is None) or (
             not isinstance(enter, int) or not isinstance(leave, int)
         ):
             return None
-        list = [index for index in range(enter, leave + 1)]
-        if target_time in list:
+        if enter <= target_time and target_time <= leave:
             times += 1
     return times
